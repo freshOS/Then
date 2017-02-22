18ce13cc44454156091db3feb66c81dc953734dc
Sacha Durand Saint Omer <sachadso@gmail.com>
2017-02-22 07:50:14 +0100

    Adds race/any + Tests

---
+++ Source/Promise+Race.swift
@@ -0,0 +1,37 @@
+//
+//  Promise+Race.swift
+//  then
+//
+//  Created by Sacha Durand Saint Omer on 22/02/2017.
+//  Copyright © 2017 s4cha. All rights reserved.
+//
+
+import Foundation
+
+public func race<T>(_ promises: Promise<T>...) -> Promise<T> {
+    return race(promises)
+}
+
+public func any<T>(_ promises: Promise<T>...) -> Promise<T> {
+    return race(promises)
+}
+
+private func race<T>(_ promises: [Promise<T>]) -> Promise<T> {
+    return Promise { resolve, reject in
+        var done = false
+        var errorCount = 0
+        for p in promises {
+            p.then { t in
+                if !done {
+                    resolve(t)
+                    done = true
+                }
+            }.onError { _ in
+                errorCount += 1
+                if errorCount == promises.count {
+                    reject(PromiseDefaultError())
+                }
+            }
+        }
+    }
+}
