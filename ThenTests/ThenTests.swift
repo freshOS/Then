925d240094e73117cf4c1e9e10868ca452256d15
S4cha <sachadso@gmail.com>
2019-09-30 09:25:56 +0200

    Update ThenTests.swift

--- ThenTests/ThenTests.swift
+++ ThenTests/ThenTests.swift
@@ -73,7 +73,7 @@ class ThenTests: XCTestCase {
             print("name :\(name)")
             thenExpectation.fulfill()
         }
-        waitForExpectations(timeout: 0.5, handler: nil)
+        waitForExpectations(timeout: 0.6, handler: nil)
     }
     
     func testSynchronousChainsWorksProprely() {
