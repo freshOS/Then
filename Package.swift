7b20f33ca37c6ecc9420c4b699b641dbca504912
S4cha <sachadso@gmail.com>
2019-12-23 15:34:41 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,31 +1,21 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Then",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Then",
-            targets: ["Then"])
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v6)
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+        .library(name: "Then", targets: ["Then"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products
-        // in packages which this package depends on.
-        .target(
-            name: "Then",
-            dependencies: [],
-            path: "Source"),
-        .testTarget(
-            name: "ThenTests",
-            dependencies: ["Then"],
-            path: "ThenTests")
+        .target(name: "Then", path: "Source"),
+        .testTarget(name: "ThenTests", dependencies: ["Then"], path: "ThenTests")
     ]
 )
