526050d17bbf850cf038486269b135862efad9ec
Sacha DSO <sachadso@gmail.com>
2024-09-29 14:30:47 -1000

    Migrates RaceTests to Testing

--- Tests/ThenTests/RaceTests.swift
+++ Tests/ThenTests/RaceTests.swift
@@ -6,86 +6,92 @@
 //  Copyright © 2017 s4cha. All rights reserved.
 //
 
-import XCTest
+import Testing
 import Then
 
-class RaceTests: XCTestCase {
+@Suite
+struct RaceTests {
     
-    func testAsyncRaceFirstArrivesFirst() {
-        let e = expectation(description: "")
-        let p1 = Promise<String> { r, _ in
-            waitTime(0.1) {
-                r("1")
+    func asyncRaceFirstArrivesFirst() async{
+        let result = await withCheckedContinuation { continuation in
+            let p1 = Promise<String> { r, _ in
+                waitTime(0.1) {
+                    r("1")
+                }
+            }
+            let p2 = Promise<String> { r, _ in
+                waitTime(0.3) {
+                    r("2")
+                }
+            }
+            Promises.race(p1, p2).then { s in
+                continuation.resume(returning: s)
             }
         }
-        let p2 = Promise<String> { r, _ in
-            waitTime(0.3) {
-                r("2")
-            }
-        }
-        Promises.race(p1, p2).then { s in
-            e.fulfill()
-            XCTAssertEqual(s, "1")
-        }
-        waitForExpectations(timeout: 0.3, handler: nil)
+        #expect(result == "1")
     }
     
-    func testSyncRaceFirstArrivesFirst() {
-        let e = expectation(description: "")
-        let p1 = Promise("1")
-        let p2 = Promise("2")
-        Promises.race(p1, p2).then { s in
-            e.fulfill()
-            XCTAssertEqual(s, "1")
+    @Test
+    func testSyncRaceFirstArrivesFirst() async {
+        let result = await withCheckedContinuation { continuation in
+            let p1 = Promise("1")
+            let p2 = Promise("2")
+            Promises.race(p1, p2).then { s in
+                continuation.resume(returning: s)
+            }
         }
-        waitForExpectations(timeout: 0.3, handler: nil)
+        #expect(result == "1")
     }
     
     struct TestRaceError: Error {}
     
-    func testAsyncRaceWithFirsFailingFails() {
-        let e = expectation(description: "")
-        let p1 = Promise<String>.reject(TestRaceError())
-        let p2 = Promise<String> { r, _ in
-            waitTime(2) {
-                r("2")
+    @Test
+    func asyncRaceWithFirsFailingFails() async {
+        let result = await withCheckedContinuation { continuation in
+            let p1 = Promise<String>.reject(TestRaceError())
+            let p2 = Promise<String> { r, _ in
+                waitTime(2) {
+                    r("2")
+                }
+            }
+            Promises.race(p1, p2).onError { error in
+                guard error as? TestRaceError != nil else {
+                    Issue.record("testRecoverCanThrowANewError failed")
+                    return
+                }
+                continuation.resume(returning: "done")
             }
         }
-        Promises.race(p1, p2).onError { error in
-            guard error as? TestRaceError != nil else {
-                XCTFail("testRecoverCanThrowANewError failed")
-                return
-            }
-            e.fulfill()
-        }
-        waitForExpectations(timeout: 0.3, handler: nil)
+        #expect(result == "done")
     }
     
-    func testAsyncRaceWithSecondFailingSuceeds() {
-        let e = expectation(description: "")
-        let p1 = Promise("1")
-        let p2 = Promise<String> { r, _ in
-            waitTime(2) {
-                r("2")
+    @Test
+    func asyncRaceWithSecondFailingSuceeds() async {
+        let result = await withCheckedContinuation { continuation in
+            let p1 = Promise("1")
+            let p2 = Promise<String> { r, _ in
+                waitTime(2) {
+                    r("2")
+                }
+            }
+            Promises.race(p1, p2).then { s in
+                continuation.resume(returning: s)
             }
         }
-        Promises.race(p1, p2).then { s in
-            e.fulfill()
-            XCTAssertEqual(s, "1")
-        }
-        waitForExpectations(timeout: 0.3, handler: nil)
+        #expect(result == "1")
     }
     
-//    func testRaceFailsIfAllFail() {
-//        let e = expectation(description: "")
-//        let p1 = Promise<String>.reject()
-//        let p2 = Promise<String>.reject()
-//        Promises.race(p1, p2).then { _ in
-//            XCTFail("testRaceFailsIfAllFail failed")
-//        }.onError { _ in
-//            e.fulfill()
-//        }
-//        waitForExpectations(timeout: 0.3, handler: nil)
-//    }
-    
+    @Test
+    func raceFailsIfAllFail() async {
+        let result = await withCheckedContinuation { continuation in
+            let p1 = Promise<String>.reject()
+            let p2 = Promise<String>.reject()
+            Promises.race(p1, p2).then { _ in
+                Issue.record("testRaceFailsIfAllFail failed")
+            }.onError { _ in
+                continuation.resume(returning: "error")
+            }
+        }
+        #expect(result == "error")
+    }
 }
